93c0468b9fdf58889b88eb587515f2c900d15211
C. Annable <christopher.annable@gmail.com>
2021-06-20 16:02:41 -0400

    Enhance init.sh with a few things from the unifi.init script.

--- init.sh
+++ init.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+# ------------------------------------------------------------------------------
+# Unifi Network Controller Starter Script
+# ------------------------------------------------------------------------------
+
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+
+UNIFI_DATADIR=/usr/lib/unifi/data
+UNIFI_RUNDIR=/usr/lib/unifi/run
+UNIFI_LOGDIR=/usr/lib/unifi/logs
+
+
+# JVM Options
+JVM_OPTS=""
+JVM_OPTS="${JVM_OPTS} -Djava.awt.headless=true"
+JVM_OPTS="${JVM_OPTS} -Dfile.encoding=UTF-8"
+JVM_OPTS="${JVM_OPTS} -Xmx${JVM_MAXHEAP}"
+JVM_OPTS="${JVM_OPTS} -Dunifi.datadir=${UNIFI_DATADIR}"
+JVM_OPTS="${JVM_OPTS} -Dunifi.rundir=${UNIFI_RUNDIR}"
+JVM_OPTS="${JVM_OPTS} -Dunifi.logdir=${UNIFI_LOGDIR}"
+JVM_OPTS="${JVM_OPTS} -cp /usr/share/java/commons-daemon.jar:/usr/lib/unifi/lib/ace.jar"
 
 
 # Ensure data directories are set up properly
-for dname in run data logs; do
-
-    dpath="/usr/lib/unifi/${dname}"
+for dpath in "${UNIFI_DATADIR}" "${UNIFI_RUNDIR}" "${UNIFI_LOGDIR}"; do
 
     mkdir -p "${dpath}"
 
@@ -17,4 +34,5 @@ for dname in run data logs; do
     find "${dpath}" -type f ! -perm 0640 -exec chmod 0640 \{\} \;
 done
 
-su -s /bin/bash -c "java -Djava.awt.headless=true -Dfile.encoding=UTF-8 -Xmx${JVM_MAXHEAP} -jar /usr/lib/unifi/lib/ace.jar start" unifi
+# Start Unifi
+su -s /bin/bash -c "java ${JVM_OPTS} -jar /usr/lib/unifi/lib/ace.jar start" unifi
